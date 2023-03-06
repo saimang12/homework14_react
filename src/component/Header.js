c68228afff5f089b9287d387a68f752aa5a3bb37
saimang12@naver.com <saimang12@naver.com>
2023-03-06 14:57:35 +0900

    세팅

---
+++ src/component/Header.js
@@ -0,0 +1,19 @@
+const Header = () => {
+    return (
+        <header className="header">
+            <div className="inner">
+                <h1>
+
+                </h1>
+                <nav className="gnb">
+
+                </nav>
+                <div className="h_link">
+
+                </div>
+            </div>
+        </header>
+    )
+}
+
+export default Header;

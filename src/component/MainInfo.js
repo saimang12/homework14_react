34cdc5abcfc0c7801b880c159cb04a574e68528c
saimang12@naver.com <saimang12@naver.com>
2023-03-10 16:53:50 +0900

    2023-03-10 상세페이지

--- src/component/MainInfo.js
+++ src/component/MainInfo.js
@@ -1,34 +1,76 @@
 import { BsHandThumbsUpFill, BsFillMapFill, BsFillCheckSquareFill } from "react-icons/bs";
-import { Link } from "react-router-dom";
+import { Link, Route, Routes, useParams } from "react-router-dom";
+import styled from "styled-components";
+
+const Test = styled.div`
+    position: fixed;
+    inset: 0 0 0 0;
+    background: rgba(0,0,0,0.5);
+    z-index: 999;
+`
+const Testin = styled.div`
+    position: absolute;
+    inset:50% 0 0 50%;
+    transform:translate(-50%,-50%);
+    width: 200px;
+    height: 200px;
+    background: #000;
+`
+
+const DetailInfo = ({ MAIN_INFO }) => {
+    const { id } = useParams();
+    const detailInfo = MAIN_INFO.find(it => String(it.id) === id);
+    return (
+        <Test>
+            {
+                detailInfo &&
+                <Testin>{MAIN_INFO.title}</Testin>
+            }
+        </Test>
+    )
+}
 
 const MainInfo = ({ MAIN_INFO }) => {
     return (
-        <section className="MainInfo">
-            <div className="inner">
-                <div className="info_text">
-                    <ul>
-                        {
-                            MAIN_INFO.map((it, idx) => {
-                                return (
-                                    <li key={idx}>
-                                        <i>
-                                            <Link to={`/`}>{it.icon}</Link>
-                                        </i>
-                                        <strong>{it.title}</strong>
-                                        <p>{it.desc}</p>
-                                    </li>
-                                )
-                            })
-                        }
-                    </ul>
+        <>
+            <section className="MainInfo">
+                <div className="inner">
+                    <div className="info_text">
+                        <ul>
+                            {
+                                MAIN_INFO.map((it, idx) => {
+                                    return (
+                                        <li key={idx}>
+                                            <i>
+                                                <Link to={`/detail/${it.id}`}>{it.icon}</Link>
+                                            </i>
+                                            <strong>{it.title}</strong>
+                                            <p>{it.desc}</p>
+                                        </li>
+                                    )
+                                })
+                            }
+                        </ul>
+                    </div>
+                    <div className="info_img">
+                        <figure>
+                            <img src={process.env.PUBLIC_URL + `/image/main_info01.jpg`} alt="" />
+                        </figure>
+                    </div>
                 </div>
-                <div className="info_img">
-                    <figure>
-                        <img src={process.env.PUBLIC_URL + `/image/main_info01.jpg`} alt="" />
-                    </figure>
-                </div>
-            </div>
-        </section>
+
+
+
+            </section>
+            <Routes>
+                <Route path="/" element={null} />
+                <Route path="/detail/:id" element={
+                    <DetailInfo
+                        MAIN_INFO={MAIN_INFO} />
+                } />
+            </Routes>
+        </>
+
     )
 }
 
--- src/component/MainMovie.js
+++ src/component/MainMovie.js
@@ -7,7 +7,7 @@ const MainMovie = () => {
         width: '100%',
         playerVars: {
             // https://developers.google.com/youtube/player_parameters
-            autoplay: 1,
+            autoplay: 0,
         },
     };
     return (
